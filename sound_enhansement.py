93040f3c4cb8de4b0fee26e9b9ee4b04e8e28bf5
txmxrxrxnxi <mrn.vych@gmail.com>
2024-03-25 12:07:46 +0200

    Initial commit: Basic GUI

---
+++ sound_enhansement.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy as sp
+
+
+class SoundEnhansement:
+    @staticmethod
+    def process(data: np.ndarray):
+        return
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
